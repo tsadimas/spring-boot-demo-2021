b99eb6eece29bcbdbddcae9bcbd194219b52a3ad
Anargyros Tsadimas <tsadimas@hua.gr>
2022-01-20 10:33:22 +0200

    enabled cors

--- src/main/java/gr/hua/ds/springboot1/config/WebSecurityConfig.java
+++ src/main/java/gr/hua/ds/springboot1/config/WebSecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 
 @Configuration
@@ -36,10 +42,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //        Disable session
 //        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+        http.cors();
+
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/users").hasRole("ADMIN")
-                .antMatchers("/api/students").hasAnyRole("ADMIN","USER")
+//                .antMatchers("/api/students").hasAnyRole("ADMIN","USER")
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
@@ -52,6 +60,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/resources/**");
         web.ignoring().antMatchers("/signup");
         web.ignoring().antMatchers("/adduser");
+        web.ignoring().antMatchers("/api/students");
+
 
     }
     @Bean
@@ -59,4 +69,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
